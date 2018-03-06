388a7035804f7250c28e948e4a5c93688f18e5e2
ELOQ <beatious@gmail.com>
2018-03-06 09:11:25 -0500

    merged

--- server.js
+++ server.js
@@ -55,30 +55,18 @@ io.sockets.on('connection',
         console.log("We have a new client: " + socket.id);
         
         // When this user emits, client side: socket.emit('otherevent',some data);
-<<<<<<< HEAD
-        socket.on('chatmessage', function(data) {
-            // Data comes in as whatever was sent, including objects
-            console.log("Received: 'chatmessage' " + data);
-            client.send('/word', data.word, function () {});
-            // Send it to all of the clients
-            socket.broadcast.emit('chatmessage', data);
-        });
-        
-        
-        socket.on('disconnect', function() {
-=======
         socket.on('mouse',
             function (data) {
                 // Data comes in as whatever was sent, including objects
                 console.log("Received: 'mouse' " + data.w + " " + data.h + " " + data.r);
                 // Send it to all other clients
+                client.send('/word', data.word, function () {});
                 socket.broadcast.emit('mouse', data);
             }
         );
-
-
-        socket.on('disconnect', function () {
->>>>>>> phil
+        
+        
+        socket.on('disconnect', function() {
             console.log("Client has disconnected " + socket.id);
         });
     }
--- sketch.js
+++ sketch.js
@@ -1,32 +1,31 @@
+var myRec = new p5.SpeechRec('en-US', parseResult); // new P5.SpeechRec object
+myRec.continuous = true; // do continuous recognition
+myRec.interimResults = true; // allow partial recognition (faster, less accurate)
+
+var x, y;
+var dx, dy;
+
+
 var dummies = [];
 var xoff = 0.0;
 
-
-var socket;
-
-function setup() {
+var socket;// = io.connect();
+function setup(){
+    // graphics stuff:
     createCanvas(innerWidth, innerHeight);
-    //        frameRate(10);
+    background(255, 255, 255);
     for (var i = 0; i < 10; i++) {
         dummies[i] = new Dummy(30, 30, 30);
     }
+    socket = io.connect('https://67.207.89.207:1337');
+    // instructions:
+    textSize(48);
+    textAlign(CENTER);
 
-
-    socket = io.connect('http://45.55.233.15:2018');
-    // We make a named event called 'mouse' and write an
-    // anonymous callback function
+    myRec.start(); // start engine
     socket.on('mouse',
         function (data) {
-            // Draw a blue circle
-//            fill(0, 0, 255);
-//            noStroke();
-//            ellipse(data.x, data.y, 80, 80);
-
-
-
             var el = document.getElementById("body");
-
-            //    el.classList.remove("fade-out");
 
             dummies.push(new Dummy(data.w, data.h, data.r));
 
@@ -36,35 +35,23 @@ function setup() {
     );
 }
 
+// Receive from any event
+socket.on('chatmessage', function (data) {
+    console.log(data);
+    document.getElementById('messages').innerHTML = "" + data + 
++ "" + document.getElementById('messages').innerHTML;
+});
 
-function mousePressed() {
-    // Make a little object with mouseX and mouseY
-    var data = {
-        w: 30,
-        h: 30,
-        r: 30
-    };
-    // Send that object to the socket
-    socket.emit('mouse', data);
-}
+var sendmessage = function(message) {
+    console.log("chatmessage: " + message);
+    socket.emit('chatmessage', message);
+};
 
-
-
-
-
-//function setup() {
-//    createCanvas(innerWidth, innerHeight);
-//    //        frameRate(10);
-//    for (var i = 0; i < 10; i++) {
-//        dummies[i] = new Dummy(30, 30, 30);
-//    }
-//}
-
-function draw() {
+function draw(){
     background(255);
     fill(0);
     rect(200, 150, 50, 50);
-    for (var i = 0; i < dummies.length; i++) {
+    for (var i = 0; i < dummies.length; i++){
         dummies[i].show();
         dummies[i].move();
     }
@@ -79,43 +66,45 @@ function draw() {
         console.log(dummies[dummies.length - 1]);
         dummies[dummies.length - 1].x -= random(3);
     }
+    if (keyIsDown(ENTER)) {
+        console.log(dummies[dummies.length - 1]);
+    }
 }
-
-
-//function mousePressed() {
-//    var el = document.getElementById("body");
-//
-//    //    el.classList.remove("fade-out");
-//
-//    dummies.push(new Dummy(30, 30, 30));
-//
-//    el.classList.add("fade-out");
-//    setTimeout(fadeIn, 100);
-//}
+function mousePressed() {
+    // Make a little object with mouseX and mouseY
+    var data = {
+        w: 30,
+        h: 30,
+        r: 30
+    };
+    // Send that object to the socket
+    socket.emit('mouse', data);
+}
 
 function fadeIn() {
     var el = document.getElementById("body");
     el.classList.remove("fade-out");
 }
 
-//var xoff = 0.0;
-//
-//function draw() {
-//    background(204);
-//    xoff = xoff + 0.01;
-//    var n = noise(xoff) * width;
-//    line(n, 0, n, height);
-//}
+function parseResult()
+{
+    // recognition system will often append words into phrases.
+    var res = myRec.resultString;
+    console.log(res, '\t',dummies[dummies.length - 1]);
+    switch (res){
+        case 'left':
+            dummies[dummies.length - 1].x -= random(3);
+            break;
+        case 'right':
+            dummies[dummies.length - 1].x += random(3);
+            break;
+        case 'kill':
+            break;                    
+    }
 
-//function movement() {
-//    if (r >= 255) {
-//        r = 0;
-//    } else {
-//        r = r + 30;
-//    }
-//    setTimeout(rChange, 450);
-//}
-
+    // text(res, width/2, height/2);
+    socket.emit('result', { 'word': res });
+}
 class Dummy {
     constructor(w, h, r) {
         this.x = random(width);
